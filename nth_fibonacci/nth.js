c2c85b1d18670da11eb17a81b8036ea720d32293
Shivansh Rawat <shivanshrawat587@gmail.com>
2022-06-27 12:27:30 +0530

    nth number of fibonacci series

--- nth_fibonacci/nth.js
+++ nth_fibonacci/nth.js
@@ -1,11 +1,10 @@
 let a = parseInt(process.argv[2])
 let sum = 0
 function fibo(first,second,count=0){ 
-    if(count<=5){
+    if(count<=a){
         sum = first+second
-       fibo(second,sum,count+1)
-       console.log(first)   
-    }
-    // return first
+       fibo(second,sum,count+1) 
+    } 
+    return sum
 }fibo(1,1)
-// console.log(first)
+console.log(sum)
